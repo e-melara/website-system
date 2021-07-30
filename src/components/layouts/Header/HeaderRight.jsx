415297852fc13dcb4100fa2573af7f233f43c78b
Edwin Melara <edwin.melara.landaverde@gmail.com>
2021-07-30 08:57:07 -0600

    Modficacion de Theme

--- src/components/layouts/Header/HeaderRight.jsx
+++ src/components/layouts/Header/HeaderRight.jsx
@@ -29,7 +29,8 @@ const HeaderRight = () => {
             className="text-dark"
             href="#!"
           >
-            <svg
+            
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
               height="24"
@@ -42,7 +43,7 @@ const HeaderRight = () => {
               className="feather feather-maximize"
             >
               <path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3"></path>
-            </svg>
+            </svg> */}
           </a>
         </li>
         <li className="profile-nav onhover-dropdown p-0 me-0">
